2838c4c8d94c3eea21c7bc4fdc733f0d195c9dc0
Tam To Tran <tamtotran5667@gmail.com>
2019-08-27 07:23:19 +0700

    completed exercise 02 lab01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Exercise01Component } from './exercise01/exercise01.component';
+import { Exercise02Component } from './exercise02/exercise02.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercise01Component
+    Exercise01Component,
+    Exercise02Component
   ],
   imports: [
     BrowserModule,
