6aa9f063fd5b9c187b1226bb77033efd70a06fb7
Tam To Tran <tamtotran5667@gmail.com>
2019-08-27 16:21:49 +0700

    copy component exercise 04 && fixed UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RatingModule } from 'ng-starrating'
+import { Ng2SearchPipeModule } from 'ng2-search-filter'
+import {FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Exercise01Component } from './exercise01/exercise01.component';
@@ -13,6 +16,8 @@ import { NavChangeExerciseComponent } from './nav-change-exercise/nav-change-exe
 import { NavHeaderExercise03Component } from './nav-header-exercise03/nav-header-exercise03.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { Exercise05Component } from './exercise05/exercise05.component';
+import { Exercise04Component } from './exercise04/exercise04.component';
+import { Exercise04DetailProductComponent } from './exercise04-detail-product/exercise04-detail-product.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,17 +29,24 @@ import { Exercise05Component } from './exercise05/exercise05.component';
     NavChangeExerciseComponent,
     NavHeaderExercise03Component,
     HomePageComponent,
-    Exercise05Component
+    Exercise05Component,
+    Exercise04Component,
+    Exercise04DetailProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RatingModule,
+    Ng2SearchPipeModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomePageComponent},
       { path: 'exercise/1', component: Exercise01Component},
       { path: 'exercise/2', component: Exercise02Component},
       { path: 'exercise/3', component: Exercise03Component},
       { path: 'exercise/3/cart', component: Exercise03CartComponent},
+      { path: 'exercise/4', component: Exercise04Component},
+      { path: 'exercise/4/products/:productId', component: Exercise04DetailProductComponent},
       { path: 'exercise/5', component: Exercise05Component},
       { path: '**', component: WrongPathComponent}
     ],  { enableTracing: true })
