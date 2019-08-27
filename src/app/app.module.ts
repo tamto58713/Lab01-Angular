fde60f037d67692e6eeddd5f5832114c95a7f513
Tam To Tran <tamtotran5667@gmail.com>
2019-08-27 09:36:09 +0700

    completed exercise 03 && fix vertical exercise 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Exercise01Component } from './exercise01/exercise01.component';
 import { Exercise02Component } from './exercise02/exercise02.component';
+import { Exercise03Component } from './exercise03/exercise03.component';
+import { Exercise03CartComponent } from './exercise03-cart/exercise03-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Exercise01Component,
-    Exercise02Component
+    Exercise02Component,
+    Exercise03Component,
+    Exercise03CartComponent
   ],
   imports: [
     BrowserModule,
