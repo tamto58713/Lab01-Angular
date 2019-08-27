d8b64a5a7cb1a5b95a3da635defbfb02d3a399e5
Tam To Tran <tamtotran5667@gmail.com>
2019-08-27 10:17:32 +0700

    make router for lab01 exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,40 @@ import { Exercise01Component } from './exercise01/exercise01.component';
 import { Exercise02Component } from './exercise02/exercise02.component';
 import { Exercise03Component } from './exercise03/exercise03.component';
 import { Exercise03CartComponent } from './exercise03-cart/exercise03-cart.component';
-
+import { RouterModule} from '@angular/router';
+import { WrongPathComponent } from './wrong-path/wrong-path.component';
+import { NavChangeExerciseComponent } from './nav-change-exercise/nav-change-exercise.component';
+import { NavHeaderExercise03Component } from './nav-header-exercise03/nav-header-exercise03.component'
 @NgModule({
   declarations: [
     AppComponent,
     Exercise01Component,
     Exercise02Component,
     Exercise03Component,
-    Exercise03CartComponent
+    Exercise03CartComponent,
+    WrongPathComponent,
+    NavChangeExerciseComponent,
+    NavHeaderExercise03Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: Exercise03Component, 
+      children: [
+        {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: '/exercise/3'
+        }
+    ]
+      },
+      { path: 'exercise/1', component: Exercise01Component},
+      { path: 'exercise/2', component: Exercise02Component},
+      { path: 'exercise/3', component: Exercise03Component},
+      { path: 'exercise/3/cart', component: Exercise03CartComponent},
+      { path: '**', component: WrongPathComponent}
+    ],  { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
